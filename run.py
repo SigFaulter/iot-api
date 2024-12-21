c1c65b5def63a5e99a28923553b078e7e75837d2
SigFaulter <107132173+SigFaulter@users.noreply.github.com>
2024-12-21 14:29:11 +0100

    add cors configuration

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 from api import create_api
 from api.db import init_db
+from flask_cors import CORS
 
 app = create_api()
+CORS(app, origins=["http://localhost:5000"])
 
 if __name__ == '__main__':
     app.run(debug=True)
