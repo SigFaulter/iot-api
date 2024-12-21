c6e83d6986fe10f3f6a2b130b1758455507829a2
SigFaulter <107132173+SigFaulter@users.noreply.github.com>
2024-12-21 14:30:56 +0100

    change cors configuration to allow everything, testing only

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from api.db import init_db
 from flask_cors import CORS
 
 app = create_api()
-CORS(app, origins=["http://localhost:5000"])
+CORS(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
